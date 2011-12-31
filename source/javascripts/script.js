c1e15edabaa546d7fa5d12705b8558980134856d
Jon Koon <jondkoon@gmail.com>
2011-12-31 13:09:01 -0500

    Added some new portfolio images, tookout the grayscale for prototyping, added instructor to the teaching page

--- source/javascripts/script.js
+++ source/javascripts/script.js
@@ -1,78 +1,62 @@
 $(function(){
 
-   useJquery();
-   $('.portfolio-image').hover(function(){
-       grayscale.reset(this);
-   }, function(){
-       grayscale(this);
-   });
-   grayscale($('.portfolio-image'));
+   //$('.portfolio-image').hover(function(){
+   //    grayscale.reset(this);
+   //}, function(){
+   //    grayscale(this);
+   //});
+   //grayscale($('.portfolio-image'));
 
-   function useJquery(){
-       var images = $('.image-crop');
-       images.each(function(){
-           var $this = $(this);
-           var img = $this.find("img");
-           var imageCSS = {
-               height: img.height(),
-               width: img.width(),
-               'margin-left': img.css('margin-left'),
-               'margin-top': img.css('margin-top')
-           };
-           var position = $this.position();
-           var originalCSS = {
-               top: position.top,
-               left: position.left,
-               width: $this.width(),
-               height: $this.height(),
-               'z-index': 0
-           };
-           //set position explicitly so animation works properly
-           $this.css(originalCSS);
+   var images = $('.image-crop');
+   var pieceInfos = $('.piece-info');
+   images.each(function(index){
+       var $this = $(this);
+       var pieceInfo = pieceInfos.eq(index);
+       var img = $this.find("img");
+       var imageCSS = {
+           'margin-left': img.css('margin-left'),
+           'margin-top': img.css('margin-top')
+       };
+       var position = $this.position();
+       var originalCSS = {
+           top: position.top,
+           left: position.left,
+           width: $this.width(),
+           height: $this.height(),
+           'z-index': 0
+       };
+       //set position explicitly so animation works properly
+       $this.css(originalCSS);
 
+       $this.click(function(){
            var activeCropCSS = {
                top: 0,
                left: 0,
-               width: imageCSS.width,
-               height: imageCSS.height,
+               width: img.width(),
+               height: img.height()
            };
 
            var activeImgCSS = {
                'margin-left': '0px',
                'margin-top': '0px',
            };
-
-           $this.click(function(){
-               if($this.is('.active-image')){
-                   $this.animate(originalCSS, 500, function(){
-                       $this.removeClass('active-image');
-                   });
-                   img.animate(imageCSS, 500);
-               } else {
-                   $this.addClass('active-image').animate(activeCropCSS, 500);
-                   img.animate(activeImgCSS, 500);
-               }
-           });
+           if($this.is('.active-image')){
+               $this.animate(originalCSS, 500, function(){
+                   $this.removeClass('active-image');
+               });
+               img.animate(imageCSS, 500);
+               pieceInfo.slideUp();
+           } else {
+               $this.addClass('active-image').animate(activeCropCSS, 500);
+               img.animate(activeImgCSS, 500, function(){
+               });
+               pieceInfo.slideDown();
+           }
        });
-        images.css({
-            position: 'absolute'
-        });
-   }
-
-   function useCSS(){
-        $(".image-crop").click(function(){
-            var $this = $(this);
-            $this.toggleClass('active-image')
-       }).each(function(){
-           var $this = $(this);
-           var position = $this.position();
-           $this.css({
-               top: position.top,
-               left: position.left,
-           });
-       }).css({
-           position: "absolute"
-       });
-   }
-
+   });
+   //after setting each images calculated top and left property
+   //lets set their position to absolute so animation will work well
+   images.css({
+       position: 'absolute'
+   });
 });
