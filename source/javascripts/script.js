ff086df7e1883df2fbc99b8a76a24e37eafeee7d
Jon Koon <jondkoon@gmail.com>
2012-01-04 00:33:39 -0500

    resized images

--- source/javascripts/script.js
+++ source/javascripts/script.js
@@ -1,41 +1,17 @@
 $(function(){
 
-    //A Linked List Node
-    //Expects a data object that will be stored with the node
-    function LLNode(data) {
-        this.data = data || null;
-        this.next = null;
-        this.prev = null;
-    
-        this.addAfter = function(node){
-            node.next = this.next;
-            if(node.next !== null){
-                this.next.prev = node;
-            }
-            this.next = node;
-            node.prev = this;
-        };
-    };
-
-   //$('.portfolio-image').hover(function(){
-   //    grayscale.reset(this);
-   //}, function(){
-   //    grayscale(this);
-   //});
-   //grayscale($('.portfolio-image'));
-
-   var images = $('.image-crop');
+   var imageCrops = $('.image-crop');
    var pieceInfos = $('.piece-info');
-   images.each(function(index){
+   imageCrops.each(function(index){
        var $this = $(this);
        var pieceInfo = pieceInfos.eq(index);
-       var img = $this.find("img");
+       var images = $this.find("img");
        var closeLink = $this.find(".close-link");
        var nextImg = $this.find(".next-image");
        var prevImg = $this.find(".prev-image");
        var imageCSS = {
-           'margin-left': img.css('margin-left'),
-           'margin-top': img.css('margin-top')
+           'margin-left': images.css('margin-left'),
+           'margin-top': images.css('margin-top')
        };
        var position = $this.position();
        var originalCSS = {
@@ -48,12 +24,12 @@ $(function(){
        //set position explicitly so animation works properly
        $this.css(originalCSS);
 
-       img.each(function(index){
+       images.each(function(index){
             var curImage = $(this);
-            var nextImage = img.eq(index+1);
-            if(nextImage.length === 0) nextImage = img.eq(0);
-            var prevImage = img.eq(index-1);
-            if(prevImage.length === 0) prevImage = img.eq(img.length -1);
+            var nextImage = images.eq(index+1);
+            if(nextImage.length === 0) nextImage = images.eq(0);
+            var prevImage = images.eq(index-1);
+            if(prevImage.length === 0) prevImage = images.eq(images.length -1);
             curImage.data('next', nextImage);
             curImage.data('prev', prevImage);
        });
@@ -61,18 +37,18 @@ $(function(){
        nextImg.add(prevImg).click(function(){
            var next = "next";
            if(this === prevImg.get(0)) next = "prev";
-           var curImg = img.filter(":visible");
+           var curImg = images.filter(":visible");
            var nextImg = curImg.data(next);
            curImg.hide();
            nextImg.show();
        });
 
-       img.add(closeLink).click(function(){
+       images.add(closeLink).click(function(){
            var activeCropCSS = {
                top: 0,
                left: 0,
-               width: img.width(),
-               height: img.height()
+               width: images.width(),
+               height: images.height()
            };
 
            var activeImgCSS = {
@@ -83,23 +59,19 @@ $(function(){
                $this.animate(originalCSS, 500, function(){
                    $this.removeClass('active-image');
                });
-               img.animate(imageCSS, 500);
+               images.animate(imageCSS, 500);
                pieceInfo.slideUp();
            } else {
                $this.addClass('active-image').animate(activeCropCSS, 500);
-               img.animate(activeImgCSS, 500, function(){
+               images.animate(activeImgCSS, 500, function(){
                });
                pieceInfo.slideDown();
            }
        });
    });
-   //after setting each images calculated top and left property
+   //after setting each imageCrops calculated top and left property
    //lets set their position to absolute so animation will work well
-   images.css({
+   imageCrops.css({
        position: 'absolute'
    });
-
-   setTimeout(function(){
-       //images.eq(0).find(".close-link").trigger('click');
-   }, 500);
 });
